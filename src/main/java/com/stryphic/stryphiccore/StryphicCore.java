2ef93206e739163d491796b3bd557fff715c419a
DeaSTL <jhartway99@gmail.com>
2019-11-09 23:57:03 -0500

    Added forge project

---
+++ src/main/java/com/stryphic/stryphiccore/StryphicCore.java
@@ -0,0 +1,30 @@
+package com.stryphic.stryphiccore;
+
+import com.stryphic.stryphiccore.util.Reference;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class StryphicCore
+{
+
+
+    public static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
---
+++ src/main/java/com/stryphic/stryphiccore/util/Reference.java
@@ -0,0 +1,7 @@
+package com.stryphic.stryphiccore.util;
+
+public class Reference {
+    public static final String MODID = "stryphiccore";
+    public static final String NAME = "Stryphic Core";
+    public static final String VERSION = "0.0.1";
+}
